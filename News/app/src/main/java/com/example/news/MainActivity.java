2ce8f2a262b3b3891b5a74c1b83f41e6c549ec59
yfm <yangfm@zhiyun-tech.com>
2023-02-21 14:12:02 +0800

    init

---
+++ News/app/src/main/java/com/example/news/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.news;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadData();
+    }
+
+    protected void loadData() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://v2.alapi.cn/api/new/toutiao?start=1&token=LwExDtUWhF3rH5ib")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+                        Log.d("fmlog", response.body().string());
+//                    }
+//                });
+            }
+        });
+    }
+}
