123a4d4b838da0a01f68de2bae3333fb41340e1e
yfm <yangfm@zhiyun-tech.com>
2023-02-23 14:28:22 +0800

    -

--- News/app/src/main/java/com/example/news/Joke.java
+++ News/app/src/main/java/com/example/news/Joke.java
@@ -1,10 +1,8 @@
 package com.example.news;
 
 public class Joke {
-//    public String code;
-//    public String msg;
-//    public String time;
-//    public String data;
-    public String name;
-    public String age;
+    public String code;
+    public String msg;
+    public String time;
+    public Data data;
 }
--- News/app/src/main/java/com/example/news/MainActivity.java
+++ News/app/src/main/java/com/example/news/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -15,34 +17,58 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import java.util.function.Consumer;
+
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        loadData();
-        parse();
+        textView = findViewById(R.id.text);
+        requestData();
     }
 
-    void parse() {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        requestData();
+        return super.onTouchEvent(event);
+    }
+
+    protected void requestData() {
+        loadData(joke -> {
+            Log.d("fmlog", "success");
+            if(joke.data != null && joke.data.message != null) {
+                textView.setText(joke.data.message);
+            } else {
+                textView.setText("null");
+            }
+        }, error-> {
+            textView.setText("failed");
+            Log.d("fmlog", "failed");
+        });
+    }
+
+    Joke parse(String json) {
         Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Joke> jokeAdapter = moshi.adapter(Joke.class);
-        String json = "{\"name\":\"John\",\"age\":30}";
         try {
             Joke joke = jokeAdapter.fromJson(json);
-            Log.d("fmlog", joke.name + " " + joke.age);
+            return joke;
         } catch (IOException e) {
             // 处理异常
             e.printStackTrace();
             // 或者显示错误消息，重新加载数据等
         }
+        return null;
     }
 
-    protected void loadData() {
+    protected void loadData(Consumer<Joke> onSuccess, Consumer<String> onFailure) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("https://collect.xmwxxc.com/collect/joke/")
@@ -51,19 +77,24 @@ public class MainActivity extends AppCompatActivity {
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                e.printStackTrace();
+//                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onFailure.accept(e.getMessage());
+                    }
+                });
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 final String res = response.body().string();
+                final Joke joke = parse(res);
 
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Log.d("fmlog", "请求成功");
-                        Log.d("fmlog", res);
-                        Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+                        onSuccess.accept(joke);
                     }
                 });
             }
